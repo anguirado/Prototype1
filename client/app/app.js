d047c9b8322504e684b513a30b79e27680b72578
anguirado <anguirado@gmail.com>
2015-10-18 22:16:15 -0400

    Second test. Only blank space

--- client/app/app.js
+++ client/app/app.js
@@ -42,6 +42,7 @@ angular.module('pymio2YeomanApp', [
     };
   })
 
+
   .run(function ($rootScope, $location, Auth) {
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$stateChangeStart', function (event, next) {
