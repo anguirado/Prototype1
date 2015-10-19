43feff322f76a177ff9eb6e97e4124559fe506d8
anguirado <anguirado@gmail.com>
2015-10-18 21:05:54 -0400

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('pymio2YeomanApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
